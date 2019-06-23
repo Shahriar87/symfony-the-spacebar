bedaea220063c06af717a2444fc35175e6180240
Innovator <shahriar.firoz@gmail.com>
2019-06-23 15:46:18 -0500

    Twig show it and do it

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -4,10 +4,11 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ArticleController
+class ArticleController extends AbstractController
 {
     /**
      * @Route("/")
@@ -22,10 +23,18 @@ class ArticleController
      */
     public function show($slug)
     {
-        return new Response(sprintf(
-            'Future page to show the article: %s',
-            $slug
-        ));
+        $comments = [
+            'This is my comments 1: I am awesome!',
+            'This is my 2nd comments: What should I say?',
+            'Third Comment: I\'m going on an all-asteroid diet',
+        ];
+
+        return $this->render(
+            'article/show.html.twig', [
+            'title' => ucwords(str_replace('-', ' ', $slug)),
+            'comments' => $comments,
+        ]
+        );
     }
 
 }
